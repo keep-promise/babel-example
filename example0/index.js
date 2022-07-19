b32fc70db71cf2eeda07bb55598b568a161cf592
keep-promise <64321089+keep-promise@users.noreply.github.com>
2022-07-19 10:40:22 +0800

    Create index.js

---
+++ example0/index.js
@@ -0,0 +1,28 @@
+var babelCore = require("@babel/core");
+
+var sourceCode = `let fn = (a) => a+ 1`;
+
+var options = {
+  //是否生成解析的代码
+  code: true,
+  //是否生成抽象语法树
+  ast: true,
+  //是否生成sourceMap
+  sourceMaps: true,
+  plugins: [],
+  presets: [],
+};
+
+babelCore.transform(sourceCode, options, function (err, result) {
+  console.log("source code：");
+  console.log(sourceCode);
+  console.log("==========");
+  console.log("result code：");
+  console.log(result.code);
+  console.log("==========");
+  console.log("result map：");
+  console.log(result.map);
+  console.log("==========");
+  console.log("result ast：");
+  console.log(result.ast);
+});
