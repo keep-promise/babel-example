8f2f1d9612fcf6d4c49f2eef693a1c7b085e2403
keep-promise <64321089+keep-promise@users.noreply.github.com>
2022-07-21 11:21:05 +0800

    Create index.js

---
+++ example4/index.js
@@ -0,0 +1,7 @@
+### Babel插件和预设
+
+Babel的插件大致可以分为语法插件和转换插件：
+
+语法插件：作用于解析阶段，使得babel能够解析更多的语法，官方的语法插件以babel-plugin-syntax开头。
+
+转换插件：在转换这一步把源码转换并输出，官方的转换插件以babel-plugin-transform（正式）或 babel-plugin-proposal（提案）开头。
