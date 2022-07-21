00fdd20a940e23e2585a56fcc0f2ede3718810e1
keep-promise <64321089+keep-promise@users.noreply.github.com>
2022-07-21 10:40:32 +0800

    Create index.js

---
+++ example3/index.js
@@ -0,0 +1,16 @@
+let fun = () => console.log("hello babel", this);
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  say() {
+    console.log(`my name is：${this.name}`);
+  }
+}
+
+Object.assign({},{})
+
+
+const tom = new Person("tom");
+tom.say();
