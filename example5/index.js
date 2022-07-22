6560aae76e7c4b75848fa70fc0be854c3cbcf864
keep-promise <64321089+keep-promise@users.noreply.github.com>
2022-07-22 10:04:45 +0800

    Create index.js

---
+++ example5/index.js
@@ -0,0 +1,12 @@
+// import "core-js";
+
+Object.assign({}, {});
+
+[(1, 2, 3, 5)].filter(function (value) {
+  return value > 3;
+});
+
+let promise = new Promise((resolve, reject) => {
+  resolve(1);
+});
+
